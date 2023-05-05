0b6c900234261553fce036cc0d11fe496c5561a6
Aniket Kumar <kraniket123654@gmail.com>
2023-05-05 21:53:44 +0530

    added trailer

--- src/components/MediaComponent.js
+++ src/components/MediaComponent.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function MediaComponent(props) {
   return (
-      <video width="100%" height="100%" controls poster="../img/aot_thumbnail1.png">
-        <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4" />
+      <video controlsList="nodownload"  width="100%" height="100%" controls poster="../img/aot_thumbnail1.png">
+        <source src="../vid/trailer.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
   );
