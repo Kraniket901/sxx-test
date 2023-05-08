9c8b89971ca10f974a41acbddc79fd98836a8e13
mohit <mohitd2288@gmail.com>
2023-05-08 22:24:43 +0530

    signout

--- src/layout/WalletPopUp.js
+++ src/layout/WalletPopUp.js
@@ -3,10 +3,10 @@ import { connect } from "react-redux";
 import { walletToggle } from "../redux/actions/siteSettings";
 import { useSession, signIn, signOut, getSession } from "next-auth/react";
 import Link from "next/link";
-import Avatar from "../components/Avatar";
+import { useRouter } from "next/router";
 
 const WalletPopUp = ({ walletToggle, wallet }) => {
-  
+  const router = useRouter();
   const { data: session, status } = useSession();
 
   return (
@@ -31,13 +31,16 @@ const WalletPopUp = ({ walletToggle, wallet }) => {
             <h3>{session?.user.name || "Aniket Kumar"}</h3>
           </div>
           {/* <button>Edit Name</button> */}
-          <button>
-            <Link href="/avatar" style={{listStyle:"none", color:"white", background: 'red'}}>Edit Avatar</Link>
+          <button
+            onClick={() => {
+              router.push("/avatar");
+            }}
+          >
+            Edit Avatar
           </button>
 
-            <button onClick={() => signOut()}>Logout</button>
-            {/* <button onClick={() => signIn("google")}>Login</button> */}
-
+          <button onClick={() => signOut()}>Signout</button>
+          {/* <button onClick={() => signIn("google")}>Login</button> */}
         </div>
       </div>
     </Fragment>
