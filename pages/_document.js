29281af52f4158dbb26be3c0f802183893902a7c
Aniket Kumar <kraniket123654@gmail.com>
2023-05-11 22:06:28 +0530

    yoo

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,16 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-N9GD15L2JS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-N9GD15L2JS');
+</script>
+        </Head>
       <body>
         <Main />
         <NextScript />
