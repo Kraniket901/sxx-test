62642459d9bade5dc858da09a0c3cb1a976c7378
mohit <mohitd2288@gmail.com>
2023-05-11 23:45:16 +0530

    views update

--- pages/api/view.js
+++ pages/api/view.js
@@ -3,14 +3,14 @@ import prisma from "../../prisma/prisma";
 export default async function handler(req, res) {
   const data = await prisma.view.findUnique({
     where: {
-      id: "645d1920116c79358d5482d8",
+      id: "645bb9acf39706824d4ebb02",
     },
   });
   if (req.method === "POST") {
     const newCount = data.count + 1;
     await prisma.view.update({
       where: {
-        id: "645d1920116c79358d5482d8",
+        id: "645bb9acf39706824d4ebb02",
       },
       data: {
         count: newCount,
