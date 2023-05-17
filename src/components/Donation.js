39f662a2ae08e0a7da63b20b058ed5d729966cb0
harshdew02 <harshdewangan032@gmail.com>
2023-05-17 21:34:34 +0530

    last img problem resolved

--- src/components/Donation.js
+++ src/components/Donation.js
@@ -1,52 +1,59 @@
-import { Card, Grid, Row, Text } from "@nextui-org/react";
+import { Card, Grid, Row } from "@nextui-org/react";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import { checkout } from "../../checkout";
 import Link from "next/link";
+import { Modal, Button, Image, Text } from "@nextui-org/react";
+import React from "react";
+import { useState } from "react";
 
 const Donation = () => {
-  const { data: session } = useSession();
-  const email = session?.user.email;
-  const router = useRouter();
 
+  const [cno , setcno] = useState(-1);
+  const handler = (i) => setcno(i);
+
+  const closeHandler = () => {
+    setcno(-1);
+    console.log("closed");
+  };
   // avatarList smh
   const avatarList = [
-    {
+    { 
       title: "Founding Titan Donation",
       price: "price_1N5n6WSFDWmXpjvIukkLLJrq",
       img: "img/titans/Founding_Titan.webp",
     },
-    {
+    { 
       title: "Attack Titan Donation",
       price: "price_1N5n7eSFDWmXpjvIEoXtnekv",
       img: "img/titans/Attack_Titan.webp",
     },
-    {
+    { 
       title: "Armored Titan Donation",
       price: "price_1N5nA6SFDWmXpjvIKh6EduWI",
       img: "img/titans/Armored_Titan.webp",
     },
-    {
+    { 
       title: "Female Titan Donation",
       price: "price_1N4I7wSFDWmXpjvInYPZaFGl",
       img: "img/titans/Female_Titan.webp",
     },
-    {
+    { 
       title: "Beast Titan Donation",
       price: "price_1N5nBxSFDWmXpjvIuWaxRH49",
       img: "img/titans/Beast_Titan.webp",
     },
-    {
+    { 
       title: "Colossal Titan Donation",
       price: "price_1N4IAWSFDWmXpjvI91fZtgR3",
       img: "img/titans/Colossal_Titan.webp",
     },
-    {
+    { 
       title: "Jaw Titan Donation",
       price: "price_1N4ICHSFDWmXpjvIh6Nh5enw",
       img: "img/titans/Jaw_Titan.webp",
     },
-    {
+    { 
       title: "Cart Titan Donation",
       price: "price_1N4ID4SFDWmXpjvI2MaepC2O",
       img: "img/titans/Cart_Titan.webp",
@@ -57,33 +64,6 @@ const Donation = () => {
       img: "img/titans/War_Hammer.webp",
     },
   ];
-
-  // handle avatar list
-  async function handleAvatar(img) {
-    await fetch("/api/avatar", {
-      method: "post",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      //make sure to serialize your JSON body
-      body: JSON.stringify({
-        email: email,
-        img: img,
-      }),
-    })
-      .then((res) => {
-        //do something awesome that makes the world a better place
-        router.push("/");
-        setTimeout(() => {
-          router.reload();
-        }, 500);
-      })
-      .catch((err) => {
-        // catch them errors
-        console.log(err);
-      });
-  }
 
   return (
     <div data-aos="fade-up" className="container">
@@ -112,44 +92,66 @@ const Donation = () => {
                   // bg: '$yellow100',
                 },
               }}
-              className="donationCard glitch"
+              className="donationCard"
             >
-              <button
-                onClick={() => {
-                  checkout({
-                    lineItems: [
-                      {
-                        price: item.price,
-                        quantity: 1,
-                      },
-                    ],
-                  });
-                }}
-              >
-                <>
-                  <Card.Body css={{ p: 0 }}>
-                    <Card.Image
-                      src={item.img}
-                      objectFit="cover"
-                      width="100%"
-                      // height={140}
-                      alt={item.title}
+              <div>
+                <button onClick={()=>{handler(index)}}>
+                  <>
+                    <Card.Body css={{ p: 0 }}>
+                      <Card.Image
+                        src={item.img}
+                        objectFit="cover"
+                        width="100%"
+                        // height={140}
+                        alt={item.title}
+                      />
+                    </Card.Body>
+                    <Card.Footer css={{ justifyItems: "flex-start" }}>
+                      <Row justify="center" align="center">
+                        <Text
+                          h4
+                          size={20}
+                          css={{ m: 0 }}
+                          style={{ fontFamily: "font2" }}
+                        >
+                          {item.title}
+                        </Text>
+                      </Row>
+                    </Card.Footer>
+                  </>
+                </button>
+                <Modal
+                  scroll
+                  width="600px"
+                  aria-labelledby="modal-title"
+                  aria-describedby="modal-description"
+                  open={(cno == index)?true:false} onClose={closeHandler}
+                >
+                  <Modal.Header>
+                    <Text id="modal-title" size={18}>
+                      {item.title}
+                    </Text>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <Image
+                    showSkeleton
+                    src={item.img}
+                    width={400}
+                    height={400}
                     />
-                  </Card.Body>
-                  <Card.Footer css={{ justifyItems: "flex-start" }}>
-                    <Row justify="center" align="center">
-                      <Text
-                        h4
-                        size={20}
-                        css={{ m: 0 }}
-                        style={{ fontFamily: "font2" }}
-                      >
-                        {item.title}
-                      </Text>
-                    </Row>
-                  </Card.Footer>
-                </>
-              </button>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button
+                      auto
+                      flat
+                      color="error"
+                      onPress={closeHandler}
+                    >
+                      Close
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </div>
             </Card>
           </Grid>
         ))}
