b05a7f6139f62370688ba3840e20ac4c5133bb6a
Aniket Kumar <kraniket123654@gmail.com>
2023-05-23 15:56:39 +0530

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,15 @@ import store from "../src/redux/store";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Script from "next/script";
+import * as gtag from "../src/lib/gtag";
 
-function MyApp({ Component, pageProps: { session, ...pageProps }  }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  const router = useRouter();
+
   useEffect(() => {
     AOS.init({
       easing: "ease-out-sine",
@@ -15,32 +20,65 @@ function MyApp({ Component, pageProps: { session, ...pageProps }  }) {
       offset: 0,
     });
   }, []);
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <SessionProvider session={session}>
-    <Provider store={store}>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
+      <Provider store={store}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet"
+          />
+          {/* !Google Fonts */}
+          {/* Styles */}
+          <link
+            type="text/css"
+            rel="stylesheet"
+            href="/css/plugins.css?ver=4.1"
+          />
+          <link
+            type="text/css"
+            rel="stylesheet"
+            href="/css/style.css?ver=4.1"
+          />
+          <link rel="shortcut icon" href="/img/favicon.ico" />
+          <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-N9GD15L2JS"
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-N9GD15L2JS', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
         />
-        {/* !Google Fonts */}
-        {/* Styles */}
-        <link
-          type="text/css"
-          rel="stylesheet"
-          href="/css/plugins.css?ver=4.1"
-        />
-        <link type="text/css" rel="stylesheet" href="/css/style.css?ver=4.1" />
-        <link rel="shortcut icon" href="/img/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-    </Provider>
+        </Head>
+        <Component {...pageProps} />
+      </Provider>
     </SessionProvider>
   );
 }
