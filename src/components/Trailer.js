eea3c5b2f49a9ec1dd562def938763c122bcb990
Aniket Kumar <kraniket123654@gmail.com>
2023-06-17 23:22:43 +0530

    bug4

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -9,7 +9,7 @@ const Trailer = () => {
               <div className="blog__item">
                 <div style={{ paddingTop: "56%", position: "relative" }}>
                   <iframe
-                    src="https://player.vdocipher.com/v2/?otp=20160313versASE3239VovgdDDIfI3JUAyUasqAzC3UClDII3c1qL7AXWr9rmb0T&playbackInfo=eyJ2aWRlb0lkIjoiYmU0OTVkMzc5YzU5NDE4MzhkZTg5NTE4ZGI4MTY2MDEifQ==&player=&player=&player=lRb3vWGa7Ip9LIb5"
+                    src="https://player.vdocipher.com/v2/?otp=20160313versASE3239VovgdDDIfI3JUAyUasqAzC3UClDII3c1qL7AXWr9rmb0T&playbackInfo=eyJ2aWRlb0lkIjoiYmU0OTVkMzc5YzU5NDE4MzhkZTg5NTE4ZGI4MTY2MDEifQ==&player=lRb3vWGa7Ip9LIb5"
                     style={{
                       border: "0",
                       maxWidth: "100%",
